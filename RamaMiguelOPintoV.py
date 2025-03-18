0be1d982f2f6181dc6cc053ab933a5c4b760d999
MiguelOscarPintoV <pintovillamizarmiguel@gmail.com>
2025-03-18 12:36:16 -0400

    Tablas de los 10 estados con mayor incendios
    Creación de la tabla del estado de Florida

--- RamaMiguelOPintoV.py
+++ RamaMiguelOPintoV.py
@@ -134,7 +134,28 @@ WHERE
                                               """, con)
 
 
+#Florida
 
+df_tiempo_de_contencion_de_los_incendios_en_FL = pd.read_sql_query("""SELECT
+    LOCAL_FIRE_REPORT_ID AS "Reportes de Incendios",
+    FIRE_YEAR AS "Año",
+    STATE AS Estado,
+    DISCOVERY_DOY AS "Día del descubrimiento del incendio",
+    CONT_DOY AS "Día de la contención del incendio",
+    CASE
+        WHEN ABS(DISCOVERY_DOY - CONT_DOY) = 0 THEN 1
+        ELSE ABS(DISCOVERY_DOY - CONT_DOY)
+    END AS "Tardanza (en Días) de la contención del incendio",
+    CASE
+        WHEN ABS(DISCOVERY_DOY - CONT_DOY) = 0 THEN 1
+        ELSE 0
+    END AS "Incendios contenidos el mismo día"
+FROM Fires
+WHERE
+    STATE = 'FL'
+    AND CONT_DOY IS NOT NULL
+    AND LOCAL_FIRE_REPORT_ID IS NOT NULL
+                                              """, con)
 
 
 
