5ebbb99018fc78e4026e99aeb713fb44fa687d9e
MiguelOscarPintoV <pintovillamizarmiguel@gmail.com>
2025-03-18 11:13:17 -0400

    Importación de librerías y conexión con la BBDD
    Importación de librerías y conexión con la BBDD

---
+++ RamaMiguelOPintoV.py
@@ -0,0 +1,26 @@
+#importación de librerías
+
+import pandas as pd
+import plotly.express as px
+import numpy as np
+import sqlite3
+
+
+#Conexción con la base de datos limpia (los top 10 de estados con mayores reportes de incendio del 2005 al 2015)
+
+con = sqlite3.connect(r"c:\Datos\Clase de computación\Base de datos Compu II\FPA_FOD_20170508.sqlite\BBDD limpiaTrabajo final\FPA_FOD_20170508.sqlite")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
