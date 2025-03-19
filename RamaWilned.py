917ea60d4c62a4073953068ae5a84b1fc52b05af
MiguelOscarPintoV <pintovillamizarmiguel@gmail.com>
2025-03-19 14:28:36 -0400

    Gráfico de Nueva York
    Creación del gráfico de Nueva York

--- RamaWilned.py
+++ RamaWilned.py
@@ -79,3 +79,61 @@ if not df_estado.empty:
 else:
 
     print(f"No hay datos disponibles para el estado: {estado_especifico}.")
+
+
+
+    
+    estado_especifico = 'NY'  
+
+df_estado = df[df['STATE'] == estado_especifico]
+
+
+
+
+if not df_estado.empty:
+
+   
+
+    df_estado['Promedio_Acres'] = df_estado['Promedio_Acres'].round(2)
+
+
+    
+
+    plt.figure(figsize=(12, 6))
+
+    bars = plt.bar(df_estado['FIRE_YEAR'].astype(str), df_estado['Promedio_Acres'], color='#0F1185')  
+
+
+    plt.title(f'Promedio de Acres Quemados en Nueva York por Año')
+
+    plt.xlabel('Año de Incendio')
+
+    plt.ylabel('Promedio de Acres Quemados')
+
+    plt.xticks(rotation=45)
+
+    plt.tight_layout()
+
+
+    
+
+    for bar in bars:
+
+        yval = bar.get_height()  
+
+        plt.annotate(np.round(yval, decimals=2),  
+
+                     (bar.get_x() + bar.get_width() / 2, yval),  
+
+                     ha='center', va='bottom',  
+
+                     xytext=(0, 5),  
+
+                     textcoords='offset points')
+
+
+    plt.show()
+
+else:
+
+    print(f"No hay datos disponibles para el estado: {estado_especifico}.")
